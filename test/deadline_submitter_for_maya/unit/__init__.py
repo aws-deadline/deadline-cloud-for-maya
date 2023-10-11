fbb4cd01f6586373a8158f7787e00d8d152bdad0
Esteban Papp <81431996+amznestebanpapp@users.noreply.github.com>
2023-10-11 15:44:57 -0700

    easier fix
    Signed-off-by: Esteban Papp <81431996+amznestebanpapp@users.noreply.github.com>

--- test/deadline_submitter_for_maya/unit/__init__.py
+++ test/deadline_submitter_for_maya/unit/__init__.py
@@ -1,10 +1,11 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 
 import sys
-from unittest.mock import MagicMock, Mock
+from unittest.mock import MagicMock
 
 # we must mock maya and UI code
 mock_modules = [
+    "deadline.client.ui.deadline_credentials_status",
     "maya",
     "maya.api",
     "maya.api.OpenMaya",
@@ -24,13 +25,6 @@ mock_modules = [
 
 for module in mock_modules:
     sys.modules[module] = MagicMock()
-
-# Mock the call to DeadlineCredentialsStatus.getInstance that happens at the module level
-# in submit_job_to_deadline_dialog.py. That call is done at the module level to gather the
-# status before the dialog is opened.
-from deadline.client.ui.deadline_credentials_status import DeadlineCredentialsStatus  # noqa: E402
-
-DeadlineCredentialsStatus.getInstance = Mock(return_value=None)
 
 
 def configure_mocks():
