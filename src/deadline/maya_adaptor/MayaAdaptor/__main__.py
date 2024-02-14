45be3384b08d195f33f64f1f4c98df8fac78239e
Mark Wiebe <399551+mwiebe@users.noreply.github.com>
2024-02-13 18:57:30 -0800

    feat!: Create a script to build adaptor packaging artifacts (#87)
    The breaking change is due to updating the adaptor runtime dependency to
    0.4
    
    - Also deprecate the binary name MayaAdaptor, and add the name
      maya-openjd
    - Update the adaptor runtime dependency to 0.4, and fix up socket
      path into server path
    - Update the submitter to provide both Rez and Conda packages metadata.
      The Rez package names are the same for now to keep backwards
      compatibility.
    - Reduce code coverage requirement from 45 to 44, CI on one of the
      Python versions was a smidgeon below.
    
    Signed-off-by: Mark Wiebe <399551+mwiebe@users.noreply.github.com>

--- src/deadline/maya_adaptor/MayaAdaptor/__main__.py
+++ src/deadline/maya_adaptor/MayaAdaptor/__main__.py
@@ -11,7 +11,7 @@ __all__ = ["main"]
 _logger = logging.getLogger(__name__)
 
 
-def main():
+def main(reentry_exe=None):
     _logger.info("About to start the MayaAdaptor")
 
     package_name = vars(sys.modules[__name__])["__package__"]
@@ -19,13 +19,14 @@ def main():
         raise RuntimeError(f"Must be run as a module. Do not run {__file__} directly")
 
     try:
-        EntryPoint(MayaAdaptor).start()
+        EntryPoint(MayaAdaptor).start(reentry_exe=reentry_exe)
     except Exception as e:
         _logger.error(f"Entrypoint failed: {e}")
-        sys.exit(1)
+        return 1
 
     _logger.info("Done MayaAdaptor main")
+    return 0
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
