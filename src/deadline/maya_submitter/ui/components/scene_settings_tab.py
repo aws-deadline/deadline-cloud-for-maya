9cd50bfc35c5088ed16211ecad044321c47409b7
Morgan Epp <60796713+epmog@users.noreply.github.com>
2024-10-28 13:30:28 -0500

    fix: frame override does not enable frame list in Maya 2025
    Signed-off-by: Morgan Epp <60796713+epmog@users.noreply.github.com>

--- src/deadline/maya_submitter/ui/components/scene_settings_tab.py
+++ src/deadline/maya_submitter/ui/components/scene_settings_tab.py
@@ -200,4 +200,4 @@ class SceneSettingsWidget(QWidget):
         """
         Set the activated/deactivated status of the Frame override text box
         """
-        self.frame_override_txt.setEnabled(state == Qt.Checked)
+        self.frame_override_txt.setEnabled(Qt.CheckState(state) == Qt.Checked)
