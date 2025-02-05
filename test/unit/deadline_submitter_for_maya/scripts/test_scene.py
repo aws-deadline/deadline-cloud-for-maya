f216e103736acbf1f8e0cf53a2b3d4105948f0bd
Jair Ruiz <167156521+jairaws@users.noreply.github.com>
2025-02-05 20:01:05 +0000

    fix: OutputPath is read from the scene in the submitter
    Signed-off-by: Jair Ruiz <167156521+jairaws@users.noreply.github.com>

--- test/unit/deadline_submitter_for_maya/scripts/test_scene.py
+++ test/unit/deadline_submitter_for_maya/scripts/test_scene.py
@@ -1,12 +1,11 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 
 from typing import Optional
-
 import pytest
+import os
+from unittest.mock import patch, Mock
 
-from deadline.maya_submitter.scene import (
-    FrameRange,
-)
+from deadline.maya_submitter.scene import FrameRange, Scene
 
 
 class TestFrameRange:
@@ -42,3 +41,37 @@ class TestFrameRange:
             assert fr_repr == f"{start}-{stop}"
         else:
             assert fr_repr == f"{start}-{stop}:{step}"
+
+
+class TestScene:
+
+    @patch("deadline.maya_submitter.scene.maya.cmds")
+    def test_project_path(self, mock_maya_cmds: Mock) -> None:
+        Scene.project_path()
+
+        mock_maya_cmds.workspace.assert_called_once_with(query=True, rootDirectory=True)
+
+    @patch.object(Scene, "project_path")
+    @patch("deadline.maya_submitter.scene.maya")
+    def test_output_path_with_images_suffix(self, mock_maya: Mock, mock_project_path: Mock) -> None:
+        test_images_dir: str = "test_images_dir"
+        test_project_path: str = "test_project_path"
+        mock_maya.mel.eval.return_value = test_images_dir
+        mock_project_path.return_value = test_project_path
+
+        output_path: str = Scene.output_path()
+
+        assert output_path == os.path.join(test_project_path, test_images_dir)
+
+    @patch.object(Scene, "project_path")
+    @patch("deadline.maya_submitter.scene.maya")
+    def test_output_path_without_images_suffix(
+        self, mock_maya: Mock, mock_project_path: Mock
+    ) -> None:
+        test_project_path: str = "test_project_path"
+        mock_maya.mel.eval.return_value = None
+        mock_project_path.return_value = test_project_path
+
+        output_path: str = Scene.output_path()
+
+        assert output_path == test_project_path
