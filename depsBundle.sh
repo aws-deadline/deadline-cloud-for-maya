f3a11e876cd1fb76e7ab37f4726a59cf40c62a45
Alex Tran <167144297+AlexTranAmz@users.noreply.github.com>
2025-04-08 00:48:35 -0700

    chore: Fix installers and scripts for release
    Signed-off-by: Alex Tran <167144297+AlexTranAmz@users.noreply.github.com>

--- depsBundle.sh
+++ depsBundle.sh
@@ -7,12 +7,4 @@
 
 set -xeuo pipefail
 
-python depsBundle.py
-
-rm -f dependency_bundle/deadline_cloud_for_maya_submitter-deps-windows.zip
-rm -f dependency_bundle/deadline_cloud_for_maya_submitter-deps-linux.zip
-rm -f dependency_bundle/deadline_cloud_for_maya_submitter-deps-macos.zip
-
-cp dependency_bundle/deadline_cloud_for_maya_submitter-deps.zip dependency_bundle/deadline_cloud_for_maya_submitter-deps-windows.zip
-cp dependency_bundle/deadline_cloud_for_maya_submitter-deps.zip dependency_bundle/deadline_cloud_for_maya_submitter-deps-linux.zip
-cp dependency_bundle/deadline_cloud_for_maya_submitter-deps.zip dependency_bundle/deadline_cloud_for_maya_submitter-deps-macos.zip
+python3 scripts/deps_bundle.py
